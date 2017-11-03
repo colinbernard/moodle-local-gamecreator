ef1f89be07715aa9f4e46bf0fb7b9a82db270816
Colin Bernard <colinjbernard@hotmail.com>
2017-11-02 22:04:34 -0700

    fixed minor bug with category 2 generator

--- generator/categories2.php
+++ generator/categories2.php
@@ -15,7 +15,7 @@ function create_categories2_game($foldername, $answers, $form) {
 
 	$form->save_file('question1', $CFG->dirroot . '/LOR/games/potato_categories2/versions/'.$foldername.'/question1.png');
 	$form->save_file('question2', $CFG->dirroot . '/LOR/games/potato_categories2/versions/'.$foldername.'/question2.png');
-	$form->save_file('question2', $CFG->dirroot . '/LOR/games/potato_categories2/versions/'.$foldername.'/question3.png');
+	$form->save_file('question3', $CFG->dirroot . '/LOR/games/potato_categories2/versions/'.$foldername.'/question3.png');
 	$form->save_file('question4', $CFG->dirroot . '/LOR/games/potato_categories2/versions/'.$foldername.'/question4.png');
 	$form->save_file('question5', $CFG->dirroot . '/LOR/games/potato_categories2/versions/'.$foldername.'/question5.png');
 
