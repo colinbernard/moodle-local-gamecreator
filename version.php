420732e4015b47923be6a6410188fa5d646dab9b
Colin Bernard <colinjbernard@hotmail.com>
2017-11-19 14:43:06 -0800

    updated version 1.1

--- version.php
+++ version.php
@@ -7,4 +7,4 @@ $plugin->version   = 2017102800; // The current plugin version (Date: YYYYMMDDXX
 $plugin->requires  = 2016112900; // Requires this Moodle version.
 $plugin->component = 'local_gamecreator'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v1.0';
+$plugin->release = 'v1.1';
