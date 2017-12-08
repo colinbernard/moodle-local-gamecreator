ef92402591513c08285db4a66e4ed4d928ebff44
Colin Bernard <colinjbernard@hotmail.com>
2017-12-08 15:46:10 -0800

    wrote custom renderer and template for approval process

--- classes/output/renderer.php
+++ classes/output/renderer.php
@@ -1,26 +1,31 @@
 <?php
 
-namespace local_gamecreator\output;                                                                                                         
- 
-defined('MOODLE_INTERNAL') || die;                                                                                                  
- 
-use plugin_renderer_base;  
- 
+namespace local_gamecreator\output;
+
+defined('MOODLE_INTERNAL') || die;
+
+use plugin_renderer_base;
+
 class renderer extends plugin_renderer_base {
-    /**                                                                                                                             
-     * Defer to template.                                                                                                           
-     *                                                                                                                              
-     * @param success_html $page                                                                                                      
-     *                                                                                                                              
-     * @return string html for the page                                                                                             
-     */                                                                                                                             
-    public function render_success_html($page) {                                                                                      
-        $data = $page->export_for_template($this);                                                                                  
-        return parent::render_from_template('local_gamecreator/success_html', $data);                                                         
-    } 
+    /**
+     * Defer to template.
+     *
+     * @param success_html $page
+     *
+     * @return string html for the page
+     */
+    public function render_success_html($page) {
+        $data = $page->export_for_template($this);
+        return parent::render_from_template('local_gamecreator/success_html', $data);
+    }
 
     public function render_initial_html($page) {
-        $data = $page->export_for_template($this);                                                                                  
-        return parent::render_from_template('local_gamecreator/initial_html', $data);     
-    }       
+        $data = $page->export_for_template($this);
+        return parent::render_from_template('local_gamecreator/initial_html', $data);
+    }
+
+    public function render_approval_html($page) {
+        $data = $page->export_for_template($this);
+        return parent::render_from_template('local_gamecreator/approval_html', $data);     
+    }
 }
