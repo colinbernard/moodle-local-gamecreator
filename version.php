13f47cd58ce183bdfdc6cbb8579fc724db21947d
Colin Bernard <colinjbernard@hotmail.com>
2017-12-20 17:42:34 -0800

    increased version

--- version.php
+++ version.php
@@ -3,8 +3,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017120800; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2017122000; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016112900; // Requires this Moodle version.
 $plugin->component = 'local_gamecreator'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v2.1';
+$plugin->release = 'v2.2';
