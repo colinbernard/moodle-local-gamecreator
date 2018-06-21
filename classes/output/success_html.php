341091fd7d356b85281f1f0ac3476ddec5ec4b3d
Colin Bernard <colinjbernard@hotmail.com>
2018-06-20 20:28:48 -0700

    updated games page link to lor page

--- classes/output/success_html.php
+++ classes/output/success_html.php
@@ -1,35 +1,35 @@
 <?php
 
 
-namespace local_gamecreator\output;                                                                                                         
- 
-use renderable;                                                                                                                     
-use renderer_base;                                                                                                                  
-use templatable;                                                                                                                    
-use stdClass;                                                                                                                       
- 
-class success_html implements renderable, templatable {                                                                               
+namespace local_gamecreator\output;
+
+use renderable;
+use renderer_base;
+use templatable;
+use stdClass;
+
+class success_html implements renderable, templatable {
 
     var $link = null;
     var $width = null;
-    var $height = null;                                                                                             
- 
-    public function __construct($link, $width, $height) {                                                                                        
+    var $height = null;
+
+    public function __construct($link, $width, $height) {
         $this->link = $link;
         $this->width = $width;
-        $this->height = $height;                                                                   
+        $this->height = $height;
     }
- 
+
     /**                                                                                                                             
-     * Export this data so it can be used as the context for a mustache template.                                                   
-     *                                                                                                                              
-     * @return stdClass                                                                                                             
-     */                                                                                                                             
-    public function export_for_template(renderer_base $output) {                                                                    
-        $data = new stdClass();                                                                                                     
+     * Export this data so it can be used as the context for a mustache template.
+     *
+     * @return stdClass
+     */
+    public function export_for_template(renderer_base $output) {
+        $data = new stdClass();
         $data->link = $this->link;
         $data->width = $this->width;
-        $data->height = $this->height;                                                             
-        return $data;                                                                                                               
+        $data->height = $this->height;
+        return $data;
     }
 }
