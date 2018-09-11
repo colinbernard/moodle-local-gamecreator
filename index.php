62436d7abcb3eb5dde8bd9f61416648efb2135b9
colinbernard <colinjbernard@hotmail.com>
2018-09-11 10:11:29 -0700

    Improvement #4: split navigation into two separate files.

--- index.php
+++ index.php
@@ -29,80 +29,17 @@ require_login();
 
 $initial_form = new initial_form();
 
+// check if the initial form has been submitted
+if ($fromform = $initial_form->get_data()) {
 
-// check is there is no game selected or if start screen is desired.
-if (is_null(handler::get_current_game()) || isset($_GET['start'])) {
+	// set the current game
+	handler::set_current_game($fromform->gametype);
+	redirect(new moodle_url("/local/gamecreator/create.php"));
 
-	// check if the initial form has been submitted
-	if ($fromform = $initial_form->get_data()) {
-
-		// set the current game
-		handler::set_current_game($fromform->gametype);
-
-		// then display the form for the current game
-		$game_form = handler::get_current_game()->display_first_form();
-
-	} else {
-		show_initial_form();
-	}
-
-// there is a game selected
 } else {
-
-	$game = handler::get_current_game();
-	$game_form = $game->get_current_form(handler::get_custom_data()); // use custom data if this is second form
-
-
-	// the game form was cancelled
-	if ($game_form->is_cancelled()) {
-
-	  // If there is a previous form then show it
-	  if ($game->display_previous_form()) {
-
-	  } else { // else show the initial form
-
-			// reset the current game
-			handler::reset_current_game();
-			show_initial_form();
-
-	  }
-
-		// the game form has been submitted
-	} else if ($fromform = $game_form->get_data()) {
-
-	  // if there is another form then show that form and send it custom data
-	  if ($game->display_next_form($fromform)) {
-			// store session data
-			handler::set_custom_data($fromform);
-		// else generate the game
-	  } else {
-
-			if ($game->requires_POST_data) {
-				$link = $game->generate($_POST, $game_form);
-			} else {
-				$link = $game->generate($fromform, $game_form);
-			}
-
-	  	$renderable = new \local_gamecreator\output\success_html($link, $game->width, $game->height);
-	  	echo $success_output->render($renderable);
-
-	  	$info = format_text(get_string($game->get_current_info(), 'local_gamecreator'), FORMAT_MARKDOWN);
-	  	echo $OUTPUT->box($info);
-	  	$game_form->display();
-			?></div><?php
-	  }
-
-		// show the game form
-	} else {
-
-		$info = format_text(get_string($game->get_current_info(), 'local_gamecreator'), FORMAT_MARKDOWN);
-		echo $OUTPUT->box($info);
-		$game_form->display();
-
-	}
-
-
+	show_initial_form();
 }
+
 
 function show_initial_form() {
 	global $OUTPUT;
