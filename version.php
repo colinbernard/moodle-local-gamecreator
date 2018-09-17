2f793825e77f6e3853fb160ab5403d3d1c9c6ae0
colinbernard <colinjbernard@hotmail.com>
2018-09-17 16:22:09 -0700

    Added description of memory game to initial html. Increased version -> 3.0

--- version.php
+++ version.php
@@ -3,8 +3,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018010200; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2018091700; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016112900; // Requires this Moodle version.
 $plugin->component = 'local_gamecreator'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v2.4';
+$plugin->release = 'v3.0';
