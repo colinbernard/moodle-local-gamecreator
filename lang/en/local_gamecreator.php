4da7401213651661f49bdbb62d8fb8b52381789b
colinbernard <colinjbernard@hotmail.com>
2019-02-04 14:29:03 -0800

    Capitalized comments in lang file

--- lang/en/local_gamecreator.php
+++ lang/en/local_gamecreator.php
@@ -1,9 +1,9 @@
 <?php
-// access
+// Access.
 $string['gamecreator:viewplugin'] = "Use the Game Creator plugin.";
 
 
-// general
+// General.
 $string['pluginname'] = 'WCLN Game Creator';
 $string['heading'] = 'WCLN Game Creator';
 $string['gametype_select'] = 'Select a game type';
@@ -29,7 +29,7 @@ $string['too_long'] = 'Too long!';
 $string['create'] = 'Create';
 
 
-// balloons
+// Balloons.
 $string['balloonsinfo1'] = 'You have selected the game type "Balloons", fill out the following information to continue creating your own version of the game. View the help buttons if you are unclear on how to fill out the form.';
 $string['balloonsinfo2'] = 'Enter question data for your Balloons game. Do NOT include the answer as one of the options. The options MUST be different than the supplied answer. View the help buttons if you are unclear on how to fill out the form.';
 $string['numlevels'] = 'Number of levels';
@@ -52,10 +52,10 @@ $string['answerinoptions'] = 'Do not include the answer as one of the options!';
 $string['duplicate'] = 'Duplicate options are not allowed!';
 $string['toolong'] = 'Too long! Please restrict to 6 characters or less.';
 
-// arrange
+// Arrange.
 $string['arrangeinfo'] = 'You have selected the game type "Arrange", input a unique folder name (will not be visible to the user) and select four images in the correct order. The first image being the correct image to place at the top, and the last image being the correct image to place in the bottom box. The images must be of type .jpg or .png, and should be approximately 200x100 pixels.';
 
-// categories2 and 3
+// Categories2 and 3
 $string['categories2info'] = 'You have selected the game type "Categories2", input a unique folder name (will not be visible to the user). Input the correct answers in the text box. For example if the answers are "category1, category2, category1, category1, category2", you would input "12112". Select two images for the categories (size 195x108), as well as 5 images for the questions (size 150x210). All images must be .png or .jpg. All images will be scaled to the listed sizes, so try to choose similar dimensions.';
 $string['categoryimage'] = 'Category image';
 $string['questionimage'] = 'Question image';
@@ -67,14 +67,14 @@ $string['answers_error'] = "There must be 5 numbers here.";
 $string['answers_error2'] = 'Only include the numbers 1 and 2.';
 $string['answers_error3'] = 'Only include the numbers 1, 2 and 3.';
 
-// spiderlove
+// SpiderLove.
 $string['spiderloveinfo'] = 'You have selected the game type "SpiderLove". Input a unique folder name (will not be visible to the user). Input 10 images total. All images must be of type .jpg or .png. The recommended image size is 160x75 pixels. The first left image will match with the first right image and so on. The images will be shuffled each time the game is loaded.';
 $string['leftimage'] = 'Left image';
 $string['rightimage'] = 'Right image';
 $string['leftheader'] = 'Left Images';
 $string['rightheader'] = 'Right Images';
 
-// venn diagram
+// Venn Diagram.
 $string['venndiagraminfo1'] = 'You have selected the game type "Venn Diagram". Input a unique version name (which will be visible to the user). Enter a short description for the game which will be shown on the start screen. Then select the number of questions and the number of questions per level.';
 $string['venndiagraminfo2'] = 'For each question you provide, also enter two options (one for each side of the Venn diagram) as well as selecting the correct answer to the question.';
 $string['right'] = 'Right';
@@ -87,22 +87,22 @@ $string['gametitle_venn_help'] = "This title should be unique. It will be displa
 $string['gametitle_venn'] = "Game title";
 $string['venn_answer'] = "Answer";
 
-// image labelling
+// Image Labelling.
 $string['imagelabelsinfo'] = 'You have selected the game type "Image Labelling". Input a unique version name (which will not be visible to the user). Also enter the number of questions (labels) that your game will include. You will be able to upload a main picture which the user will be labelling on the next form.';
 $string['imagelabelsinfo2'] = 'Upload a picture (jpg or png) that is approximately 350x350 pixels. This picture should be labelled with letters A-F. You can find a picture on Google images and then add labels using Photoshop or a similar image editing software. You will also need to enter a term (question) and select the correct answer.';
 $string['image_labels_answer'] = 'Answer';
 $string['numbuttons'] = "Number of Buttons (Labels)";
 $string['numbuttons_help'] = "This is the number of buttons at the bottom of the screen. If the image you will be using has A-F labels, you should choose to have A-F buttons at the bottom.";
 
-// invaders
+// Invaders.
 $string['invadersinfo'] = 'You have selected the game type "Invaders", fill out the following information to continue creating your own version of the game. View the help buttons if you are unclear on how to fill out the form.';
 $string['invadersinfo2'] = 'Enter question data for your Invaders game. Do NOT include the answer as one of the options. The options MUST be different than the supplied answer. View the help buttons if you are unclear on how to fill out the form.';
 
-// memory
+// Memory.
 $string['memoryinfo'] = 'You have selected the game type "Memory". You must provide 10 images (5 pairs). The images must be of type JPG. Enter a folder name to identify this game. This folder name will not be visible when playing the game.';
 $string['pair'] = 'Pair';
 
-// multiple choice
+// Multiple Choice.
 $string['multiplechoiceinfo'] = 'You have selected the game type "Multiple Choice Quiz". Enter a title and description for your quiz. You will need to provide text based questions each with 1 answer and 3 incorrect options. On this page, specify the number of questions you will be adding. You will be unable to use this tool to add more questions later or remove questions. For each question, you will be able to provide an optional question image as well.';
 $string['multiplechoiceinfo2'] = 'Provide question information. For each question section: provide the question text, JPG or PNG question image (optional), answer, and 3 incorrect options. The answer and options will be shuffled for each question so the order they are provided in makes no difference. However, the order the questions are provided in will remain the same.';
 $string['quiztitle'] = 'Quiz title';
@@ -115,7 +115,7 @@ $string['quizinstructions_error'] = 'Quiz instructions are too long!';
 $string['createquiz'] = 'Create Quiz';
 $string['questionimage'] = 'Question Image';
 
-// two sort
+// Two Sort.
 $string['twosortinfo'] = 'You have selected the game type "Two Sort". Enter a title for your game which will be displayed at the top of the game window. Specify the names of the two columns which items will be dragged into. Finally, select the number of items to be sorted.';
 $string['twosortinfo2'] = 'Provide items to be sorted.';
 $string['left_column'] = 'Left column name';
@@ -125,3 +125,5 @@ $string['item_name_help'] = 'This will be the name of the item to be sorted into
 $string['two_sort_title_help'] = 'This title will be displayed at the top of the game window.';
 $string['two_sort_number_of_items'] = 'Number of items to sort';
 $string['correct_column'] = 'Correct column';
+
+// Hidden Picture.
